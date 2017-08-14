34dd90a1cd21da05f09863e5c6cfc6af8a5b5d03
Adrian GM <adriangmweb@gmail.com>
2017-08-14 11:53:03 +0100

    Likes, Single and Comments

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,9 +4,18 @@
 // 2. copy of the current state
 
 function posts(state = [], action) {
-  console.log('The post will change');
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('liked!');
+      const i = action.index;
+      return [
+        ...state.slice(0, i), // all before the one that we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), // all after the one that we are updating
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts;
